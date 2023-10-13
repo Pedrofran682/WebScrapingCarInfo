cd8c9e29e282fd3eb31e44565e27db6f5aeaafa6
Pedrofran682 <pedrofran682@gmail.com>
2023-10-13 17:47:03 -0300

    Backup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ShopCar import GetRequests
 
 
 def main():
-    GetRequests.ExtractData(maxIdValue = 100)
+    GetRequests.ExtractData(maxIdValue = 5000)
 
 if __name__ == "__main__":
     main()
